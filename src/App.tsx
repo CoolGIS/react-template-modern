9023f52411b7c572467d3a83444ee03b958721dd
coolgis <mitooo@outlook.com>
2024-09-17 00:47:58 +0800

    refactor: 重构导航样式

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { DevTools } from 'jotai-devtools'
 import { Counter } from '@/components/Counter.tsx'
 import { Link, Outlet } from 'react-router-dom'
-import { Divider } from '@styled/jsx'
+import { Center, Divider } from '@styled/jsx'
 import { styled } from '@styled/jsx'
 import { queryCache } from '@/plugins/tanstack-query.ts'
 
@@ -17,23 +17,15 @@ function App() {
       <DevTools />
       <QueryClientProvider client={queryClient}>
         <Counter />
-        <styled.ul
-          display="flex"
-          gap="10px"
-          justifyContent="center"
-          alignItems="center"
-          p="10px"
-          m="10px"
-          border="1px solid #ccc"
-          rounded="md"
-          bg="white"
-        >
-          <li>
-            <Link to={'/home'}>主页</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>关于</Link>
-          </li>
+        <styled.ul p="10px" m="10px" border="1px solid #ccc" rounded="md" bg="white">
+          <Center gap="10px">
+            <li>
+              <Link to={'/home'}>主页</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>关于</Link>
+            </li>
+          </Center>
         </styled.ul>
         <Divider />
         <div>
