aeaf435bfb2840a389f14cb4261419c3920140b3
coolgis <mitooo@outlook.com>
2024-09-16 17:00:38 +0800

    feat: 路由懒加载

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,9 @@
 import { Center } from '@styled/jsx'
 
-export function Error() {
+export function NotFound() {
   return (
     <Center h="50vh" fontSize="32px" fontWeight="bold" color="red.500">
-      出错了！
+      页面不存在！
     </Center>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,29 +1,41 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '@/App.tsx'
-import { Error } from '@/pages/Error.tsx'
 import { Home } from '@/pages/Home.tsx'
-import { About } from '@/pages/About.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: 'home',
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: 'home',
+          element: <Home />
+        },
+        {
+          path: 'about',
+          async lazy() {
+            const { About } = await import('@/pages/About.tsx')
+            return { Component: About }
+          }
+        }
+      ]
+    },
+    {
+      path: '*',
+      async lazy() {
+        const { NotFound } = await import('@/pages/NotFound.tsx')
+        return { Component: NotFound }
       }
-    ]
+    }
+  ],
+  {
+    basename: import.meta.env.BASE_URL
   }
-])
+)
 
 export default router
