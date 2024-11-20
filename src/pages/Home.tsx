cb939eee15169b23f9c3953834495cbcc194a793
haoj <haoj@geoscene.com.cn>
2024-11-20 14:01:14 +0800

    feat: 引入react-use工具库

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
 import { Center } from '@styled/jsx'
+import { useLifecycles } from 'react-use'
 
 export function Home() {
+  // react-use示例
+  useLifecycles(
+    () => {
+      console.log('Home mounted')
+    },
+    () => {
+      console.log('Home unmounted')
+    }
+  )
+
   return (
     <Center h="50vh" fontSize="32px" fontWeight="bold" color="blue.500">
       主页
